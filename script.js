51e30ee568b10acb0aeda344918571bceceba749
Sophia Castillo <Sphcastillo@gmail.com>
2020-08-10 13:11:07 -0700

    Map working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 $(document).ready(() => {
+    
 
-
-    var personalAPIKey = "AIzaSyA0rdSnRWTkL1PyhoHgsMoYfs8GhAJbJnw";
-
+    var userInfo = JSON.parse(localStorage.getItem("user")) || [];
+    console.log("var userInfo ", userInfo);
 
 
     $("#submitButton").on("click", function(event){
@@ -12,7 +12,7 @@ $(document).ready(() => {
         
 
         var name = $("#inputName").val();
-        var firstAddress = $("#inputAddress").val();
+        var address = $("#inputAddress").val();
         var cityofAddress = $("#inputCity").val();
         var stateofAddress = $("#inputState").val();
         var zipCodeofAddress = $("#inputZip").val();
@@ -20,7 +20,7 @@ $(document).ready(() => {
         var user = {
 
             name : name,
-            address1: firstAddress,
+            address: address,
             city: cityofAddress,
             state: stateofAddress,
             zip: zipCodeofAddress
@@ -29,93 +29,64 @@ $(document).ready(() => {
 
         console.log("user: ", user);
 
-        var arrayWithUserInfo = [];
-
-        var userInfo = JSON.parse(localStorage.getItem("user"));
-
-        arrayWithUserInfo = [userInfo];
-        arrayWithUserInfo.push(user);
-
-        localStorage.setItem("user", JSON.stringify(arrayWithUserInfo));
-
-        console.log("local storage: ", JSON.stringify(arrayWithUserInfo));
-        console.log("var userInfo ", userInfo);
 
 
-            
+        // userInfo = [userInfo];
+        userInfo.push(user);
 
-        var addingAddresstogether = `${firstAddress} ${cityofAddress} ${stateofAddress} ${zipCodeofAddress}`;
+        localStorage.setItem("user", JSON.stringify(userInfo));
+
+        console.log("local storage: ", JSON.stringify(userInfo));
+
+
+        var addingAddresstogether = `${address} ${cityofAddress} ${stateofAddress} ${zipCodeofAddress}`;
         console.log("full address: ", addingAddresstogether);
 
 
         //$("#theAddress").append(addingAddresstogether);
-
-            
-        var ourQueryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + addingAddresstogether + "&key=" + personalAPIKey;
-
-        $.ajax({
-
-            url: ourQueryURL,
-            method: "GET"
-
-        }).then(function(response){
-
-                if (response.status === "OK"){
-                var latitude = response.results[0].geometry.location.lat;
-                var longitude = response.results[0].geometry.location.lng;
-
-                console.log(response);
-        
-                initMap(latitude, longitude);
-
-                } else {
-                    alert("ENTER CORRECT ADDRESS");
-                }
-            
-        
-            })
-
+        window.location.replace("../VOLUNTEER_MAIN_/volunteer.html");
+    
     })
 
 
 
     // set conditionals so that the initMap runs based on if there are objects in the array.
-    var locationArr = [];
+    // var locationArr = [];
 
-    function initMap(lat, long){
-        console.log("inside the initMap function");
+    // function initMap(lat, long){
+    //     console.log("inside the initMap( ) function");
 
-        var  saguaro = {lat: lat, lng: long};
+    //     var  saguaro = {lat: lat, lng: long};
         
-        locationArr.push(saguaro);
+    //     locationArr.push(saguaro);
 
-        console.log(locationArr);
+    //     console.log(locationArr);
 
-        var map = new google.maps.Map(
+    //     var map = new google.maps.Map(
 
-        document.getElementById("map"),
-        {zoom: 16, center: saguaro}
+    //     document.getElementById("map"),
+    //     {zoom: 16, center: saguaro}
 
-        );
+    //     );
 
-        var icon = {
+    //     var icon = {
         
-            url: "https://i.pinimg.com/originals/40/d7/be/40d7bec2768bc8cfa69f6d8ed4f85c89.png", 
-            scaledSize: new google.maps.Size(width= 27, height=27), 
-            origin: new google.maps.Point(0,0), 
-            anchor: new google.maps.Point(0, 0) }
+    //         url: "https://i.pinimg.com/originals/40/d7/be/40d7bec2768bc8cfa69f6d8ed4f85c89.png", 
+    //         scaledSize: new google.maps.Size(width= 27, height=27), 
+    //         origin: new google.maps.Point(0,0), 
+    //         anchor: new google.maps.Point(0, 0) }
 
-            for (var i = 0; i < locationArr.length; i++){
-                var marker = new google.maps.Marker({
-                    position: locationArr[i],
-                    map:map,
-                    icon:icon,
-                    animation: google.maps.Animation.BOUNCE,
-                })
-            }
+    //         for (var i = 0; i < locationArr.length; i++){
+    //             var marker = new google.maps.Marker({
+    //                 position: locationArr[i],
+    //                 map:map,
+    //                 icon:icon,
+    //                 animation: google.maps.Animation.BOUNCE,
+    //             })
+    //         }
 
-    }
-
+//     }
+// }
 /*          VOLUNTEER.HTML                  */
 
     $('#sidebarCollapse').on('click', function () {
