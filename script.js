c69d33176a657bf1d662ae6223534b95b2b20212
Sophia Castillo <Sphcastillo@gmail.com>
2020-08-10 15:55:33 -0700

    markers, map, submit button

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ $(document).ready(() => {
     
 
     var userInfo = JSON.parse(localStorage.getItem("user")) || [];
-    console.log("var userInfo ", userInfo);
+    console.log("Each user and their information: ", userInfo);
 
 
     $("#submitButton").on("click", function(event){
-        console.log("submit Button pressed");
         event.preventDefault();
         
 
@@ -27,71 +26,21 @@ $(document).ready(() => {
                 
         }
 
-        console.log("user: ", user);
 
 
-
-        // userInfo = [userInfo];
         userInfo.push(user);
 
         localStorage.setItem("user", JSON.stringify(userInfo));
 
-        console.log("local storage: ", JSON.stringify(userInfo));
-
 
         var addingAddresstogether = `${address} ${cityofAddress} ${stateofAddress} ${zipCodeofAddress}`;
-        console.log("full address: ", addingAddresstogether);
 
 
-        //$("#theAddress").append(addingAddresstogether);
+
         window.location.replace("../VOLUNTEER_MAIN_/volunteer.html");
+
     
     })
 
-
-
-    // set conditionals so that the initMap runs based on if there are objects in the array.
-    // var locationArr = [];
-
-    // function initMap(lat, long){
-    //     console.log("inside the initMap( ) function");
-
-    //     var  saguaro = {lat: lat, lng: long};
-        
-    //     locationArr.push(saguaro);
-
-    //     console.log(locationArr);
-
-    //     var map = new google.maps.Map(
-
-    //     document.getElementById("map"),
-    //     {zoom: 16, center: saguaro}
-
-    //     );
-
-    //     var icon = {
-        
-    //         url: "https://i.pinimg.com/originals/40/d7/be/40d7bec2768bc8cfa69f6d8ed4f85c89.png", 
-    //         scaledSize: new google.maps.Size(width= 27, height=27), 
-    //         origin: new google.maps.Point(0,0), 
-    //         anchor: new google.maps.Point(0, 0) }
-
-    //         for (var i = 0; i < locationArr.length; i++){
-    //             var marker = new google.maps.Marker({
-    //                 position: locationArr[i],
-    //                 map:map,
-    //                 icon:icon,
-    //                 animation: google.maps.Animation.BOUNCE,
-    //             })
-    //         }
-
-//     }
-// }
-/*          VOLUNTEER.HTML                  */
-
-    $('#sidebarCollapse').on('click', function () {
-        console.log("I have been clicked");
-        $('#sidebar').toggleClass('active');
-    });
 
 })
